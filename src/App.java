fe9b9d17bc247cbf2ac353d9556c2f582528e0ea
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2021-11-03 11:20:42 -0300

    Codigo criado

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+class App {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int numero;
+
+
+    System.out.print("Digite um número positivo inteiro: ");
+    numero = scanner.nextInt();
+
+    if (numero < 0){
+      System.out.println("Número invalido");
+    }
+    else {
+      for (int i = 0; i <= numero; i++){
+        System.out.println(i);
+      }
+
+    }
+
+  
+  }
+}
